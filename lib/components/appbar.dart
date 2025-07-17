bd5ee1b71faf8b4019ddf86344c624a71ce25b83
juschmidtt <141142651+juschmidtt@users.noreply.github.com>
2025-07-17 14:29:57 -0300

    appbar

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class appBar extends StatelessWidget {
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return SliverAppBar.large(
-      title: Row(
-        children: [
-          const Icon(
-            Icons.auto_graph,
-            size: 40,
-            color: Colors.white,
+    return AppBar(
+      toolbarHeight: 120,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF3F51B5), Color(0xFF2196F3)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          Text("Rendify",
-              style: GoogleFonts.poppins(fontSize: 30, color: Colors.white), textAlign: TextAlign.center),
-        ],
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Image.asset('assets/logopi.png', height: 40),
+              const SizedBox(height: 8),
+              const Text(
+                'Rendify',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),],),)
       ),
-      centerTitle: true,
     );
   }
+  @override
+  Size get preferredSize => const Size.fromHeight(120);
 }
-
-// Row(
-//         children: [
-//           const Icon(
-//             Icons.auto_graph,
-//             size: 40,
-//             color: Colors.white,
-//           ),
-//           Text("Rendify",
-//               style: GoogleFonts.poppins(fontSize: 30, color: Colors.white), textAlign: TextAlign.center),
-//         ],
-//       ),
