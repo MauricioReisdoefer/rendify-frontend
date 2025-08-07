29bfaf4b4ebd7f67c1c02218c0149e8cbe0327b3
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-07 14:25:14 -0300

    FEAT: StockModel Created

--- lib/core/models/stock_model.dart
+++ lib/core/models/stock_model.dart
@@ -1,1 +1,23 @@
-class StockModel {}
+class StockModel {
+  final String symbol;
+  final double price;
+
+  StockModel({
+    required this.symbol,
+    required this.price,
+  });
+
+  factory StockModel.fromJson(Map<String, dynamic> json) {
+    return StockModel(
+      symbol: json['symbol'],
+      price: json['price'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'symbol': symbol,
+      'price': price,
+    };
+  }
+}
