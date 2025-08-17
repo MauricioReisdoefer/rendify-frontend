8ff7842b9083a70824de72f930ef128952f32f42
SoJoaomesmo <4540746781@estudante.sed.sc.gov.br>
2025-08-17 12:29:26 -0300

    Splash Screen e outros ícones adicionados

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Rendify",
       home: Scaffold(
         appBar: CustomAppBar(),
         body: LoginCadastroPage(),
