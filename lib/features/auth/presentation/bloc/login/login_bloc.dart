c5275d10b46660c29e56a4dffa7b69e595427384
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-29 21:40:43 -0300

    REFACTOR: Simplifying login auth feature

--- lib/features/auth/presentation/bloc/login/login_bloc.dart
+++ lib/features/auth/presentation/bloc/login/login_bloc.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 import 'login_event.dart';
 import 'login_state.dart';
 import 'package:rendify/core/models/user_model.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc({required this.authRepository}) : super(LoginState.initial()){
+  LoginBloc() : super(LoginState.initial()) {
     on<LoginNameChanged>((event, emit) {
       emit(state.copyWith(name: event.name));
     });
@@ -15,16 +17,23 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
 
     on<LoginSubmitted>((event, emit) async {
       emit(state.copyWith(isSubmitting: true, isFailure: false, isSuccess: false));
+
       try {
-        final UserModel user = await authRepository.login(
-          event.name, 
-          event.password,
+        final loginResponse = await http.post(
+          Uri.parse('http://localhost:5000/user/login'),
+          headers: {'Content-Type': 'application/json'},
+          body: jsonEncode({'name': event.name, 'password': event.password}),
         );
 
-        emit(state.copyWith(isSubmitting: false, isSuccess: true));
-      } catch (e) {
-        final int id = await authRepository.getByName(event.name);
-        emit(state.copyWith(isSubmitting: false, isFailure: true, name: "Done", id: id.toString() ?? "0"));
+        if (loginResponse.statusCode == 200) {
+          final data = jsonDecode(loginResponse.body);
+          final user = UserModel.fromJson(data['Result']);
+          emit(state.copyWith(isSubmitting: false, isSuccess: true, id: user.id.toString()));
+        } else {
+          throw UnimplementedError("Erro Inesperado");
+        }
+      } catch (_) {
+        emit(state.copyWith(isSubmitting: false, isFailure: true));
       }
     });
   }
